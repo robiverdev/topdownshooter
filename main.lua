39cb845b30a363ee45f5cabc5c8974f4ebc3764b
robiverdev <robi.verdev1@gmail.com>
2025-06-02 22:16:11 +0200

    Added player movement with WSAD keys

--- main.lua
+++ main.lua
@@ -8,9 +8,25 @@ function love.load()
     player = {}
     player.x = love.graphics.getWidth() /2
     player.y = love.graphics.getHeight() / 2
+    player.speed = 3
 end
 
 function love.update(dt)
+    if love.keyboard.isDown("d") then
+        player.x = player.x + player.speed
+    end
+
+    if love.keyboard.isDown("a") then
+        player.x = player.x - player.speed
+    end
+
+    if love.keyboard.isDown("w") then
+        player.y = player.y - player.speed
+    end
+
+    if love.keyboard.isDown("s") then
+        player.y = player.y + player.speed
+    end
 end
 
 function love.draw()
