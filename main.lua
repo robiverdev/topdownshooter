f5a0c4de791d48f30a9f9eff7ce885c820f21557
robiverdev <robi.verdev1@gmail.com>
2025-08-24 20:17:19 +0200

    added delta time, sprite rotation, player facing the mouse, enemies (zombies)

--- main.lua
+++ main.lua
@@ -8,29 +8,54 @@ function love.load()
     player = {}
     player.x = love.graphics.getWidth() /2
     player.y = love.graphics.getHeight() / 2
-    player.speed = 3
+    player.speed = 180
+    
+    zombies = {}
 end
 
 function love.update(dt)
     if love.keyboard.isDown("d") then
-        player.x = player.x + player.speed
+        player.x = player.x + player.speed*dt
     end
 
     if love.keyboard.isDown("a") then
-        player.x = player.x - player.speed
+        player.x = player.x - player.speed*dt
     end
 
     if love.keyboard.isDown("w") then
-        player.y = player.y - player.speed
+        player.y = player.y - player.speed*dt
     end
 
     if love.keyboard.isDown("s") then
-        player.y = player.y + player.speed
+        player.y = player.y + player.speed*dt
     end
 end
 
 function love.draw()
     love.graphics.draw(sprites.background, 0, 0 )
 
-    love.graphics.draw(sprites.player, player.x, player.y)
+    love.graphics.draw(sprites.player, player.x, player.y, playerMouseAngle(), nil, nil, sprites.player:getWidth()/2, sprites.player:getHeight()/2)
+
+    for i, z in ipairs(zombies) do
+        love.graphics.draw(sprites.zombie, z.x, z.y)
+    end
+end
+
+function playerMouseAngle()
+    -- add math.pi in order to "reverse" the player to the correct way
+    return math.atan2(player.y - love.mouse.getY(), player.x - love.mouse.getX()) + math.pi
+end
+
+function love.keypressed(key)
+    if key == "space" then
+        spawnZombie()
+    end
+end
+
+function spawnZombie()
+   local  zombie = {}
+   zombie.x = math.random(0, love.graphics.getWidth())
+   zombie.y = math.random(0, love.graphics.getHeight())
+   zombie.speed = 100
+   table.insert(zombies, zombie)
 end
