4dae62fb4fed31b92fc0cbfc91423e870dfe8ab7
robiverdev <robi.verdev1@gmail.com>
2025-08-25 20:32:15 +0200

    added projectiles deletion

--- main.lua
+++ main.lua
@@ -45,6 +45,13 @@ function love.update(dt)
         b.x = b.x + (math.cos(b.direction) * b.speed * dt)
         b.y = b.y + (math.sin(b.direction) * b.speed * dt)
     end
+
+    for i = #bullets, 1, -1 do 
+        local b = bullets[i]
+        if b.x < 0 or b.y < 0 or b.x > love.graphics.getWidth() or b.y > love.graphics.getHeight() then
+            table.remove(bullets, i)
+        end
+    end
 end
 
 function love.draw()
